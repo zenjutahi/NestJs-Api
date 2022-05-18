bbfbeff6c4a8d4daa27bdce97076360ba1d9c05e
zenjutahi <zenjutahi@gmail.com>
2022-05-18 19:12:51 +0300

    Auth module - signup user feature

--- src/auth/users.repository.ts
+++ src/auth/users.repository.ts
@@ -1,5 +1,14 @@
 import { EntityRepository, Repository } from 'typeorm';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 import { User } from './user.entity';
 
 @EntityRepository(User)
-export class UsersRepository extends Repository<User> {}
+export class UsersRepository extends Repository<User> {
+  async createUser(authCredentials: AuthCredentialsDto): Promise<void> {
+    const { username, password } = authCredentials;
+
+    const user = this.create({ username, password });
+
+    await this.save(user);
+  }
+}
