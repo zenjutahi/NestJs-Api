7be3ca846a5cbecefbca14a6fd86b37b2ce5e930
zenjutahi <zenjutahi@gmail.com>
2022-05-18 12:55:57 +0300

    Validations with nestJs"

--- src/tasks/dto/create-task-dto.ts
+++ src/tasks/dto/create-task-dto.ts
@@ -1,4 +1,10 @@
+import { IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateTaskDto {
+  @IsNotEmpty()
+  @IsString()
   title: string;
+
+  @IsNotEmpty()
   description: string;
 }
